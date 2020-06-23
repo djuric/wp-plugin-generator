4e92d4b4cfa31eb42f44b6083af958a3d2b6ec4a
Zarko <zarko.dorian@gmail.com>
2020-06-23 23:33:56 +0200

    feat(): Enable CORS for all requests

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const generate = require("./routes/generate");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/generate", generate);
 
 const port = process.env.PORT || 3000;
